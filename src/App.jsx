b7902d937282b0dd387853faa10e4c4ec9df579d
Mohamed Mujtaba <Mohamed.Mugtaba11@gmail.com>
2024-08-17 02:23:09 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer"
+import SideBar from "./components/SideBar"
+import Main from "./components/Main"
+
+
+
+function App() {
+  
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Main></Main>
+      <SideBar></SideBar>
+      <Footer>hi</Footer>
+    </>
+  )
+}
+
+export default App
